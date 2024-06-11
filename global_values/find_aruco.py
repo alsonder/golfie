f2ec75c1e588cfddad0a3a813e2ac2ea2590f096
Navn <team@example.com>
2024-06-11 11:07:50 +0200

    pushed newest aruco_detect almost ready

---
+++ global_values/find_aruco.py
@@ -0,0 +1,37 @@
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+import matplotlib.pyplot as plt
+
+def midpoint(x1, y1, x2, y2):
+    return ((x1 + x2) / 2, (y1 + y2) / 2)
+
+def detect_aruco(frame):
+    dictionary = aruco.getPredefinedDictionary(aruco.DICT_7X7_1000)
+    parameters = aruco.DetectorParameters()
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, dictionary, parameters=parameters)
+    if ids is not None:
+        aruco.drawDetectedMarkers(frame, corners, ids)
+        print(*corners[0])
+        midpoint_aruco = midpoint(midpoint(*corners[0],*corners[1]), midpoint(*corners[2],corners[3]))
+    return midpoint_aruco
+
+def detect_aruco_test(frame):
+    dictionary = aruco.getPredefinedDictionary(aruco.DICT_7X7_1000)
+    parameters = aruco.DetectorParameters()
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, dictionary, parameters=parameters)
+    if ids is not None:
+        aruco.drawDetectedMarkers(frame, corners, ids)
+        print(*corners[0])
+    return corners, ids, frame
+
+# Example usage
+image_path = 'global_values/test_image.png'
+frame = cv2.imread(image_path)
+if frame is None:
+    print("Failed to load the image")
+else:
+    aruco_corners, aruco_ids, annotated_frame = detect_aruco_test(frame)
+    plt.imshow(cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB))
+    plt.title('Detected ArUco markers')
+    plt.show()
--- global_values/test_file_global.py
+++ global_values/test_file_global.py
@@ -1,5 +1,10 @@
 import cv2
+
+## imports for main
 from find_goal import decide_goal_loc
+from find_aruco import detect_aruco
+## imports for main
+
 image = cv2.imread('global_values/test_image.png')
 
 # Check if the image has been correctly loaded
@@ -7,7 +12,8 @@ if image is not None: print("I see the image")
 else: print("Failed to load the image")
 
 # Temporary Values
-filler_aruco = ([15,15])
-filler_wall_corners = ([10,10],[15,300],[400,290],[404,308])
+filler_aruco = ([15,15]) # aruco
+filler_wall_corners = ([10,10],[15,300],[400,290],[404,308]) # wall corners
 
+#detect_aruco(image)
 decide_goal_loc(filler_aruco,filler_wall_corners)
